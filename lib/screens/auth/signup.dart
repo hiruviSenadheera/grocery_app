62703b284d9746634f8459cd569265a20c6248ec
hiru1999 <hiruvisenadeera@gmail.com>
2025-02-17 10:24:51 +0530

    Create another textfields

--- lib/screens/auth/signup.dart
+++ lib/screens/auth/signup.dart
@@ -47,8 +47,48 @@ class _SignupState extends State<Signup> {
                   boxShadow: 
                   [
                     BoxShadow(
-                      color: AppColors.ashColor.withOpacity(.4),
-                      offset: Offset(0, 2),
+                      color: AppColors.ashColor.withOpacity(.3),
+                      offset: Offset(0, 0),
+                      blurRadius: 10,
+                    ),
+                  ]
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Enter your name here",
+                    hintStyle: const TextStyle(
+                        color: AppColors.ashColor,
+                      ),
+                    label: const Text(
+                      "Name",
+                      style: TextStyle(
+                        color: AppColors.ashColor,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4),
+                      borderSide: const BorderSide(color: AppColors.primaryColor)
+                    )
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 5,),
+
+              //child 4
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.white,
+                  boxShadow: 
+                  [
+                    BoxShadow(
+                      color: AppColors.ashColor.withOpacity(.3),
+                      offset: Offset(0, 0),
                       blurRadius: 10,
                     ),
                   ]
@@ -56,10 +96,10 @@ class _SignupState extends State<Signup> {
                 child: TextField(
                   decoration: InputDecoration(
                     hintText: "Enter your email here",
-                    hintStyle: TextStyle(
+                    hintStyle: const TextStyle(
                         color: AppColors.ashColor,
                       ),
-                    label: Text(
+                    label: const Text(
                       "Email",
                       style: TextStyle(
                         color: AppColors.ashColor,
@@ -67,11 +107,52 @@ class _SignupState extends State<Signup> {
                     ),
                     enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide(color: Colors.white),
+                      borderSide: const BorderSide(color: Colors.white),
                     ),
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide(color: AppColors.primaryColor)
+                      borderSide: const BorderSide(color: AppColors.primaryColor)
+                    )
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 5,),
+
+              //child 5
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.white,
+                  boxShadow: 
+                  [
+                    BoxShadow(
+                      color: AppColors.ashColor.withOpacity(.3),
+                      offset: Offset(0, 0),
+                      blurRadius: 10,
+                    ),
+                  ]
+                ),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter your password here",
+                    hintStyle: const TextStyle(
+                        color: AppColors.ashColor,
+                      ),
+                    label: const Text(
+                      "Password",
+                      style: TextStyle(
+                        color: AppColors.ashColor,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4),
+                      borderSide: const BorderSide(color: AppColors.primaryColor)
                     )
                   ),
                 ),
