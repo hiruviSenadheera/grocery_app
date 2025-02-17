11a26e979311263e09a8f07e3b502834546becc4
hiru1999 <hiruvisenadeera@gmail.com>
2025-02-17 19:21:55 +0530

    Create a customText as vegetables

--- lib/screens/main/home/home.dart
+++ lib/screens/main/home/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:grocery_app/components/custom_text.dart';
 import 'package:grocery_app/utils/constants/app_assets.dart';
+import 'package:grocery_app/utils/constants/app_colors.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -18,16 +19,37 @@ class _HomeState extends State<Home> {
         padding: const EdgeInsets.symmetric(horizontal: 30),
 
         
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            SvgPicture.asset(
-              AppAssets.menuIcon
-            ),
+
+            //child 1
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SvgPicture.asset(
+                  AppAssets.menuIcon
+                ),
       
-            SvgPicture.asset(
-              AppAssets.cartIcon
+                SvgPicture.asset(
+                  AppAssets.cartIcon
+                ),
+              ],
             ),
+
+            const SizedBox(height: 25,),
+
+            //child 2
+            const CustomText(
+              "Vegetables",
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: AppColors.primaryColor,
+            ),
+
+            const SizedBox(height: 41,),
+
+            //child 3
+
           ],
         ),
       )
