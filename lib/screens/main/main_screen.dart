93bd4e536de45cdf57864be2bda4f09e162f7235
hiru1999 <hiruvisenadeera@gmail.com>
2025-02-17 15:39:03 +0530

    Create bottom navigation bar

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/utils/constants/app_colors.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -10,6 +11,47 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: Container(
+        height: 83,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              onPressed: (){}, 
+              icon: const Icon(
+                Icons.home,
+                size: 33,
+                color: AppColors.primaryColor,
+              )
+            ),
+            IconButton(
+              onPressed: (){}, 
+              icon: const Icon(
+                Icons.favorite_border,
+                size: 33,
+                color: AppColors.ashColor,
+              )
+            ),
+            IconButton(
+              onPressed: (){}, 
+              icon: const Icon(
+                Icons.search,
+                size: 33,
+                color: AppColors.ashColor,
+              )
+            ),
+            IconButton(
+              onPressed: (){}, 
+              icon: const Icon(
+                Icons.person,
+                size: 33,
+                color: AppColors.ashColor,
+              )
+            )
+          ],
+        )
+      ),
+    );
   }
 }
