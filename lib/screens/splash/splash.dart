0c1ef46a0517bde3d0aceabee06083f4553388bf
hiru1999 <hiruvisenadeera@gmail.com>
2025-02-17 09:12:20 +0530

    Add styles to splash screen

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -11,14 +11,35 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-
-          //child 1
-          Image.asset("assets/icons/logo.png"),
-
-          Text("Shop Your Daily Necessary")
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          
+          children: [
+      
+            //child 1
+            Image.asset(
+              "assets/icons/logo.png",
+              width: 331,
+              height: 225,
+              fit: BoxFit.fill,
+            ),
+      
+            const SizedBox(height: 72,),
+      
+            //child 2
+            const Text(
+              "Shop Your Daily \nNecessary",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff0FA956)
+              )
+              ),
+      
+          ],
+        ),
       ),
     );
   }
