28d789ea6bc64d99e9121da1f91f9658285424d0
hiru1999 <hiruvisenadeera@gmail.com>
2025-02-18 06:07:00 +0530

    Create a navigation for ProductDetails screen

--- lib/components/product_tile.dart
+++ lib/components/product_tile.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/components/custom_text.dart';
+import 'package:grocery_app/screens/main/product_details/product_details.dart';
 import 'package:grocery_app/utils/constants/app_assets.dart';
 import 'package:grocery_app/utils/constants/app_colors.dart';
+import 'package:grocery_app/utils/helpers/helpers.dart';
 
 class ProductTile extends StatelessWidget {
   const ProductTile({
@@ -10,65 +12,70 @@ class ProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(               
-      decoration: BoxDecoration(
-        color: Colors.amber,
-        borderRadius: BorderRadius.circular(12),
-        image: const DecorationImage(
-          image: NetworkImage(AppAssets.dummyImg),
-          fit: BoxFit.cover,
-        )
+    return InkWell(
+      onTap: () {
+        Helpers.navigateTo(context, const ProductDetails());
+      },
+      child: Container(               
+        decoration: BoxDecoration(
+          color: Colors.amber,
+          borderRadius: BorderRadius.circular(12),
+          image: const DecorationImage(
+            image: NetworkImage(AppAssets.dummyImg),
+            fit: BoxFit.cover,
+          )
+        ),
+    
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+    
+            Container(
+              padding: const EdgeInsets.all(7),
+              margin: const EdgeInsets.all(8),
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+              child: const Icon(
+                Icons.favorite_border,
+                color: AppColors.ashColor,
+              ),
+            ),
+    
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              height: 38,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: AppColors.primaryColor.withOpacity(.8),
+              ),
+    
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CustomText(
+                    "Pumpking",
+                    fontSize: 15,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+    
+                  CustomText(
+                    "Rs. 120.00",
+                    fontSize: 12,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ],
+              ),
+              
+            ),
+          ],
+        ),
+        
       ),
-
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-
-          Container(
-            padding: EdgeInsets.all(7),
-            margin: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
-            ),
-            child: const Icon(
-              Icons.favorite_border,
-              color: AppColors.ashColor,
-            ),
-          ),
-
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            height: 38,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: AppColors.primaryColor.withOpacity(.8),
-            ),
-
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CustomText(
-                  "Pumpking",
-                  fontSize: 15,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                ),
-
-                CustomText(
-                  "Rs. 120.00",
-                  fontSize: 12,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w500,
-                ),
-              ],
-            ),
-            
-          ),
-        ],
-      ),
-      
     );
   }
 }
