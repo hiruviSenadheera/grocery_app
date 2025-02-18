d3c18dc55e315a9de19fa060295fd6c92915c348
hiru1999 <hiruvisenadeera@gmail.com>
2025-02-18 08:38:18 +0530

    Create profile screen

--- lib/screens/main/profile/profile.dart
+++ lib/screens/main/profile/profile.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/components/custom_button.dart';
 import 'package:grocery_app/components/custom_text.dart';
+import 'package:grocery_app/utils/constants/app_assets.dart';
+import 'package:grocery_app/utils/constants/app_colors.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -11,10 +14,57 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: CustomText(
-        "Profile",
-        fontSize: 30,
+    return SafeArea(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+
+                //child 1
+                const CustomText(
+                  "Profile",
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.primaryColor,
+                ),
+
+                const SizedBox(height: 122,),
+
+                //child 2
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(45),
+                  child: Image.network(
+                    AppAssets.profileUrl,
+                    width: 200,
+                    height: 200,
+                  ),
+                ),
+
+                const SizedBox(height: 18,),
+
+                //child 3
+                const CustomText(
+                  "Terry Robinson",
+                  fontSize: 18,
+                ),
+
+                //child 4
+                const CustomText(
+                  "terryrobinson@gmail.com",
+                  fontSize: 13,
+                  color: AppColors.ashColor,
+                ),
+              ],
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: CustomButton(text: "Logout", onTap: (){}),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/utils/constants/app_assets.dart
+++ lib/utils/constants/app_assets.dart
@@ -13,5 +13,6 @@ class AppAssets {
   static const String cartIcon ="$iconPath/cart-icon.svg";
 
   static const dummyImg = "https://cdn.prod.website-files.com/61ddeea9c3ef6c6366040d63/65f8b47f290a5ed246c11620_6363c8deab63e770d2ee9b7b_marius-ciocirlan-T9pdHqCsyoQ-unsplash.jpeg";
+  static const profileUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqI9Uv_XhRdWiwMlmW-BkT7grJozLzYUfkiBubR3OEZW9RWmKJ7C6oDau46Vkyv_I_fPY&usqp=CAU";
 
 }
