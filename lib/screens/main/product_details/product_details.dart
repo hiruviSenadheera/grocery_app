bac1a15b819d51494f34d9a9112a9dde33f56075
hiru1999 <hiruvisenadeera@gmail.com>
2025-02-18 06:49:50 +0530

    Create bottom part of product details screen

--- lib/screens/main/product_details/product_details.dart
+++ lib/screens/main/product_details/product_details.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/screens/main/product_details/upper_section.dart';
+import 'package:grocery_app/utils/constants/app_colors.dart';
+import 'package:grocery_app/utils/helpers/size_config.dart';
 
-import '../../../components/upper_section.dart';
 
 class ProductDetails extends StatefulWidget {
   const ProductDetails({super.key});
@@ -13,9 +15,34 @@ class _ProductDetailsState extends State<ProductDetails> {
   @override
   Widget build(BuildContext context) {
 
-    return const Scaffold(
-      body: UpperSection(),
-    );
+    return Scaffold(
+      body:
+          SizedBox(
+            width: SizeConfig.w(context),
+            height: SizeConfig.h(context),
+
+            child: Stack(
+              children: [
+                const UpperSection(),
+                Positioned(
+                  top: 260,
+                  child: Container(
+                    width: SizeConfig.w(context),
+                    height: SizeConfig.h(context),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(34),
+                        topRight: Radius.circular(34),
+                      )
+                    ),
+                  ),
+                ),
+                
+              ],
+            ),
+          ),
+      );
   }
 }
 
